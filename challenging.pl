563854ad5ae1a10aea35020638c3410ffa331c14
Nero Tran <tranhuq@miamioh.edu>
2021-10-20 16:14:46 -0400

    Done.

--- challenging.pl
+++ challenging.pl
@@ -1,14 +1,81 @@
 
 %% I will not accept a submission with the T0D0 comments left in place!
 
-url("TODO: Replace this with the URL of the puzzle").
+url("https://logic.puzzlebaron.com/pdf/S388IZ.pdf").
 
-solution([["TODO: Replace this with the correct solution to the puzzle!"]]).
+solution([[1920,	'Grant',	'Cinnamon',			'Coffee maker'],       
+          [1934,	'Sandra',	'Peppermint',		'Blender'],
+          [1949,	'Ruby',		'Grape',			'Microwave'],
+          [1975,	'Kai',		'Tropical fruit',	'Toaster'],
+          [1990,	'Janiya',	'Bubblegum',		'Juice press']
+          ]).
+firstnames(['Grant', 'Sandra', 'Ruby', 'Kai', 'Janiya']).
+gum(['Cinnamon', 'Peppermint', 'Grape', 'Tropical fruit', 'Bubblegum']).
+gifts(['Coffee maker', 'Blender', 'Microwave', 'Toaster', 'Juice press']).
+years([1920, 1934, 1949, 1975, 1990]).
 
 solve(T) :-
-	% TODO: Implement this to solve the puzzle. 
-	true.
+	T = [ [1920, N1, G1, W1],       
+          [1934, N2, G2, W2],    
+          [1949, N3, G3, W3],
+          [1975, N4, G4, W4],
+          [1990, N5, G5, W5]],
+    gum(Gum), permutation([G1, G2, G3, G4, G5], Gum),
+    firstnames(Names), permutation([N1, N2, N3, N4, N5], Names),
+    gifts(Gifts), permutation([W1, W2, W3, W4, W5], Gifts),
+    clue1(T),
+    clue2(T),
+    clue3(T),
+    clue4(T),
+    clue5(T),
+    clue7(T),
+    clue6(T),
+    clue8(T), 
+    clue9(T).
 
+clue1(T) :-
+	member([Y1, _, 'Tropical fruit', _], T),
+	member([Y2, _, _, 'Juice press'], T),
+	Y1 < Y2.
+
+
+clue2(T) :-
+	member([1949, 'Ruby', _, _], T).
+
+clue3(T) :-
+	member([_, _, G1, 'Toaster'], T),
+	G1 \= 'Cinnamon'.
+
+clue4(T) :-
+	member([Y1, _, _, 'Blender'], T),
+	member([Y2, _, 'Cinnamon', _], T),
+	Y1 > Y2.
+
+clue5(T) :-
+	member([Y1, 'Sandra', _, _], T),
+	member([Y2, _, 'Cinnamon', _], T),
+	Y1 > Y2.
+
+clue6(T) :-
+	member([1975, _, 'Tropical fruit', _], T).
+
+clue7(T) :-
+	member([_, 'Grant', _, 'Coffee maker'], T);
+	member([_, 'Grant', _, 'Juice press'], T).
+
+clue8(T) :-
+	member([Y1, _, 'Cinnamon', _], T),
+	member([Y2, _, _, 'Blender'], T),
+	member([1975, _, _, _], T),
+	member([Y3, 'Janiya', _, _], T),
+	member([Y4, _, 'Grape', _], T),
+	years(Years), permutation([Y1, Y2, Y3, 1975, Y4], Years).
+
+clue9(T) :-
+	member([1949, _, _, _, 'Microwave'], T),
+	member([_, 'Sandra', 'Peppermint', _], T);
+	member([_, _, _, 'Peppermint', 'Microwave'], T);
+	member([_, 'Sandra', 'Peppermint', _], T).
 
 check :- 
 	% Confirm that the correct solution is found
--- veryhard.pl
+++ veryhard.pl
@@ -1,15 +1,94 @@
 
 %% I will not accept a submission with the T0D0 comments left in place!
 
-url("TODO: Replace this with the URL of the puzzle").
+url("https://logic.puzzlebaron.com/pdf/U355FR.pdf").
 
-solution([["TODO: Replace this with the correct solution to the puzzle!"]]).
+solution([[1100,	'Pamela',	'Opal',			'14k gold'],       
+          [1200,	'Lorraine',	'Aquamarine',	'White gold'],
+          [1300,	'Angela',	'Diamond',		'24k gold'],
+          [1400,	'Becky',	'Amethyst',		'Silver'],
+          [1500,	'Rebecca',	'Topaz',		'Palladium'],
+          [1600,	'Darla',	'Sapphire',		'Platinum']
+          ]).
+customers(['Pamela', 'Lorraine', 'Angela', 'Becky', 'Rebecca', 'Darla']).
+stones(['Opal', 'Aquamarine', 'Diamond', 'Amethyst', 'Topaz', 'Sapphire']).
+metals(['14k gold', 'White gold', '24k gold', 'Silver', 'Palladium', 'Platinum']).
+prices([1100, 1200, 1300, 1400, 1500, 1600]).
 
 solve(T) :-
-	% TODO: Implement this to solve the puzzle. 
-	true.
+	T = [ [1100, C1, S1, M1],       
+          [1200, C2, S2, M2],
+          [1300, C3, S3, M3],
+          [1400, C4, S4, M4],
+          [1500, C5, S5, M5],
+          [1600, C6, S6, M6]
+          ],
+    stones(Stones), permutation([S1, S2, S3, S4, S5, S6], Stones),
+    customers(Customers), permutation([C1, C2, C3, C4, C5, C6], Customers),
+    metals(Metals), permutation([M1, M2, M3, M4, M5, M6], Metals),
+    clue1(T),
+    clue2(T),
+    clue3(T),
+    clue4(T),
+    clue5(T),
+    clue7(T),
+    clue6(T),
+    clue8(T), 
+    clue9(T),
+    clue10(T),
+    clue11(T).
 
+clue1(T) :-
+	member([C1, _, _, 'Platinum'], T),
+	member([C2, 'Becky', _, _], T),
+	C1 > C2.
 
+clue2(T) :- 
+	member([_, 'Rebecca', 'Topaz', _], T).
+
+clue3(T) :-
+	member([1500, _, 'Topaz', _], T),
+	member([_, 'Becky', _, 'Silver'], T);
+	member([1500, _, _, 'Silver'], T);
+	member([_, 'Becky', 'Topaz', _], T).
+
+clue4(T) :- 
+	member([_, _, S, '24k gold'], T),
+	S \= 'Sapphire'.
+
+clue5(T) :-
+	member([C1, _, _, '24k gold'], T),
+	member([C2, _, 'Opal', _], T),
+	C1 is C2 + 200.
+
+clue6(T) :-
+	member([C1, 'Becky', _, _], T),
+	member([C2, _, 'Sapphire', _], T),
+	member([C3, _, _, 'Palladium'], T),
+	member([C4, _, _, 'White gold'], T),
+	member([C5, _, 'Opal', _], T),
+	member([C6, _, 'Diamond', _], T),
+	prices(Prices), permutation([C1, C2, C3, C4, C5, C6], Prices).
+
+clue7(T) :-
+	member([C, 'Pamela', _, _], T),
+	C \= 1600.
+
+clue8(T) :-
+	member([_, 'Lorraine', _, M], T),
+	M \= 'Palladium'.
+
+clue9(T) :-
+	member([_, 'Angela', 'Diamond', _], T);
+	member([_, 'Angela', 'Aquamarine', _], T).
+
+clue10(T) :-
+	member([C1, _, _, 'Silver'], T),
+	member([C2, 'Lorraine', _, _], T),
+	C1 is C2 + 200.
+
+clue11(T) :-
+	member([1400, _, 'Amethyst', _], T).
 
 check :- 
 	% Confirm that the correct solution is found
